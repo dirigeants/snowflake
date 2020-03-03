3c07955064d4f2c4db70b916c0fb192a04c543ba
bdistin <bdistin@gmail.com>
2020-03-03 16:27:07 -0600

    BigInt Snowflake Util
    From project blue with tests, and a fix or two

--- test/Util.ts
+++ test/Util.ts
@@ -1,6 +1,58 @@
 import ava from 'ava';
-import util from '../dist';
+import { Snowflake } from '../dist';
 
-ava('sample test', (test): void => {
-	test.is(util(), 'bar');
+ava('new Snowflake()', (test): void => {
+	test.plan(8);
+
+	const testID = '167383252271628289';
+	const snow = new Snowflake(testID);
+
+	test.is(snow.id, BigInt(testID)); // eslint-disable-line no-undef
+	test.is(snow.timestamp, 1459977677172);
+	test.is(snow.workerID, 0);
+	test.is(snow.processID, 0);
+	test.is(snow.increment, 1);
+	test.is(snow.binary, '0000001001010010101010100010111001011101000000000000000000000001');
+	test.deepEqual(snow.date, new Date(1459977677172));
+	test.is(snow.toString(), testID);
+});
+
+ava('Snowflake.generate(number)', (test): void => {
+	test.plan(8);
+
+	const testID = '167383252271763456';
+	const testTimestamp = 1459977677172;
+	const snow = Snowflake.generate(testTimestamp);
+
+	test.is(snow.id, BigInt(testID)); // eslint-disable-line no-undef
+	test.is(snow.timestamp, testTimestamp);
+	test.is(snow.workerID, 1);
+	test.is(snow.processID, 1);
+	test.is(snow.increment, 0);
+	test.is(snow.binary, '0000001001010010101010100010111001011101000000100001000000000000');
+	test.deepEqual(snow.date, new Date(testTimestamp));
+	test.is(snow.toString(), testID);
+});
+
+ava('Snowflake.generate(Date)', (test): void => {
+	test.plan(8);
+
+	const testID = '167383252271763457';
+	const testDate = new Date(1459977677172);
+	const snow = Snowflake.generate(testDate);
+
+	test.is(snow.id, BigInt(testID)); // eslint-disable-line no-undef
+	test.is(snow.timestamp, testDate.valueOf());
+	test.is(snow.workerID, 1);
+	test.is(snow.processID, 1);
+	test.is(snow.increment, 1);
+	test.is(snow.binary, '0000001001010010101010100010111001011101000000100001000000000001');
+	test.deepEqual(snow.date, testDate);
+	test.is(snow.toString(), testID);
+});
+
+ava('Snowflake.generate(invalid)', (test): void => {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+	// @ts-ignore
+	test.throws(() => Snowflake.generate('foo'), { instanceOf: TypeError });
 });
